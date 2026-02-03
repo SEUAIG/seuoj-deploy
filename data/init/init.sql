f2c3a4f0dbce86b1704aa5d7faee01200d69fb66
Twilight <2475911580@qq.com>
2026-02-03 17:14:31 +0800

    fix: 修改后端有关配置+数据库初始化脚本

--- data/init/init.sql
+++ data/init/init.sql
@@ -22,7 +22,8 @@ CREATE TABLE `problem`  (
                             `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                             `is_del` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
                             PRIMARY KEY (`id`) USING BTREE,
-                            UNIQUE INDEX `pid`(`pid` ASC) USING BTREE
+                            UNIQUE INDEX `pid`(`pid` ASC) USING BTREE,
+                            FULLTEXT INDEX `idx_problem_title_ft`(`title`) WITH PARSER ngram
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '题目表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -32,11 +33,10 @@ DROP TABLE IF EXISTS `problem_tag_rel`;
 CREATE TABLE `problem_tag_rel`  (
                                     `id` bigint NOT NULL AUTO_INCREMENT,
                                     `problem_id` bigint NOT NULL,
-                                    `tag_id` int NOT NULL,
+                                    `tag_id` bigint NOT NULL,
                                     `is_del` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
-                                    `active_key` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci GENERATED ALWAYS AS ((case when (`is_del` = 0) then concat(`problem_id`,_utf8mb4'#',`tag_id`) else NULL end)) STORED NULL,
                                     PRIMARY KEY (`id`) USING BTREE,
-                                    UNIQUE INDEX `uk_problem_tag_rel_active`(`active_key` ASC) USING BTREE,
+                                    UNIQUE INDEX `uk_problem_tag_rel`(`problem_id` ASC, `tag_id` ASC) USING BTREE,
                                     INDEX `idx_problem_tag_rel_problem_id`(`problem_id` ASC) USING BTREE,
                                     INDEX `idx_problem_tag_rel_tag_id`(`tag_id` ASC) USING BTREE,
                                     CONSTRAINT `fk_problem_tag_rel_problem` FOREIGN KEY (`problem_id`) REFERENCES `problem` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
@@ -71,17 +71,35 @@ CREATE TABLE `submission`  (
 ) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户提交与评测结果表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
+-- Table structure for tag_group
+-- ----------------------------
+DROP TABLE IF EXISTS `tag_group`;
+CREATE TABLE `tag_group`  (
+                          `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+                          `type` varchar(32) NOT NULL COMMENT '分组类型，algorithm/source/time/special',
+                          `name` varchar(64) NULL DEFAULT NULL COMMENT '分组名称，NULL 表示默认分组',
+                          `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                          `is_del` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
+                          PRIMARY KEY (`id`) USING BTREE,
+                          UNIQUE KEY `uk_type_name_del` (`type`, `name`, `is_del`)
+) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '标签分组表' ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
 -- Table structure for tag
 -- ----------------------------
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag`  (
-                        `id` int NOT NULL AUTO_INCREMENT,
+                        `id` bigint NOT NULL AUTO_INCREMENT,
                         `tag_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+                        `group_id` bigint NOT NULL COMMENT '分组ID，关联 tag_group 表',
                         `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                         `is_del` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
                         PRIMARY KEY (`id`) USING BTREE,
-                        UNIQUE INDEX `uk_tag_name_del`(`tag_name` ASC, `is_del` ASC) USING BTREE
+                        UNIQUE INDEX `uk_tag_name_del`(`tag_name` ASC, `is_del` ASC) USING BTREE,
+                        INDEX `idx_tag_group_id`(`group_id` ASC) USING BTREE,
+                        CONSTRAINT `fk_tag_group_id` FOREIGN KEY (`group_id`) REFERENCES `tag_group` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '题目标签表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -91,12 +109,13 @@ DROP TABLE IF EXISTS `user_info`;
 CREATE TABLE `user_info`  (
                               `id` bigint NOT NULL AUTO_INCREMENT,
                               `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '登录名',
+                              `email` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '邮箱',
                               `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT '加密密码',
                               `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
                               `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                               `is_del` tinyint(1) NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除',
                               PRIMARY KEY (`id`) USING BTREE,
-                              UNIQUE INDEX `uk_username_del`(`username` ASC, `is_del` ASC) USING BTREE
+                              UNIQUE INDEX `uk_email_del`(`email` ASC, `is_del` ASC) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户基础表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
@@ -137,39 +156,119 @@ SET FOREIGN_KEY_CHECKS = 1;
 SET FOREIGN_KEY_CHECKS = 0;
 
 INSERT INTO `problem` (id, pid, title, total_submit, total_accept)
-VALUES (1, 'p01', 'a+b', 0, 0);
+VALUES (1, 'p01', 'a+b', 0, 0),
+       (2, 'p02', '数组求和', 0, 0),
+       (3, 'p03', '最大子数组和', 0, 0),
+       (4, 'p04', '两数之和', 0, 0),
+       (5, 'p05', '二分查找', 0, 0),
+       (6, 'p06', '合并区间', 0, 0),
+       (7, 'p07', '前K大', 0, 0),
+       (8, 'p08', '迷宫最短路', 0, 0),
+       (9, 'p09', '树的深度优先遍历', 0, 0),
+       (10, 'p10', '迪杰斯特拉', 0, 0),
+       (11, 'p11', 'Floyd最短路', 0, 0),
+       (12, 'p12', '最长公共子序列', 0, 0),
+       (13, 'p13', '01背包', 0, 0),
+       (14, 'p14', '线段树', 0, 0),
+       (15, 'p15', '树状数组', 0, 0),
+       (16, 'p16', '并查集', 0, 0),
+       (17, 'p17', '拓扑排序', 0, 0),
+       (18, 'p18', '强连通分量', 0, 0),
+       (19, 'p19', '最短路径', 0, 0),
+       (20, 'p20', '字符串匹配', 0, 0),
+       (21, 'p21', '回文判断', 0, 0);
 
--- ----
+INSERT INTO `tag_group` (id, type, name, created_at, updated_at, is_del)
+VALUES (1, 'algorithm', NULL, NOW(), NOW(), 0),
+       (2, 'algorithm', '基础算法', NOW(), NOW(), 0),
+       (3, 'algorithm', '高级算法', NOW(), NOW(), 0),
+       (4, 'source', 'NOI系列赛事', NOW(), NOW(), 0),
+       (5, 'source', '经典套题', NOW(), NOW(), 0),
+       (6, 'source', '国际知名赛事', NOW(), NOW(), 0),
+       (7, 'source', '大学竞赛', NOW(), NOW(), 0),
+       (8, 'time', NULL, NOW(), NOW(), 0),
+       (9, 'special', NULL, NOW(), NOW(), 0);
 
-INSERT INTO `problem_tag_rel`
-VALUES (1, 1, 1, 0, DEFAULT);
-INSERT INTO `problem_tag_rel`
-VALUES (2, 1, 2, 0, DEFAULT);
+INSERT INTO `tag` (id, tag_name, group_id, created_at, updated_at, is_del)
+VALUES (1, '贪心', 2, NOW(), NOW(), 0),
+       (2, '动态规划', 2, NOW(), NOW(), 0),
+       (3, '图论', 3, NOW(), NOW(), 0),
+       (4, '冷门标签', 1, NOW(), NOW(), 0),
+       (5, 'NOI', 4, NOW(), NOW(), 0),
+       (6, 'NOIP', 4, NOW(), NOW(), 0),
+       (7, 'NOI Online', 4, NOW(), NOW(), 0),
+       (8, '经典套题一', 5, NOW(), NOW(), 0),
+       (9, '经典套题二', 5, NOW(), NOW(), 0),
+       (10, '经典套题三', 5, NOW(), NOW(), 0),
+       (11, 'ICPC', 6, NOW(), NOW(), 0),
+       (12, 'IOI', 6, NOW(), NOW(), 0),
+       (13, 'Google Code Jam', 6, NOW(), NOW(), 0),
+       (14, '校内赛', 7, NOW(), NOW(), 0),
+       (15, '校级选拔赛', 7, NOW(), NOW(), 0),
+       (16, '省赛', 7, NOW(), NOW(), 0),
+       (17, '2000', 8, NOW(), NOW(), 0),
+       (18, '2001', 8, NOW(), NOW(), 0),
+       (19, '2002', 8, NOW(), NOW(), 0),
+       (20, '2003', 8, NOW(), NOW(), 0),
+       (21, '交互题', 9, NOW(), NOW(), 0),
+       (22, '提交答案', 9, NOW(), NOW(), 0),
+       (23, 'O2优化', 9, NOW(), NOW(), 0);
 
--- ----
+INSERT INTO `problem_tag_rel` (id, problem_id, tag_id)
+VALUES (1, 1, 1),
+       (2, 1, 2),
+       (3, 2, 1),
+       (4, 2, 4),
+       (5, 3, 2),
+       (6, 3, 4),
+       (7, 4, 1),
+       (8, 4, 2),
+       (9, 5, 1),
+       (10, 5, 4),
+       (11, 6, 1),
+       (12, 6, 3),
+       (13, 7, 3),
+       (14, 7, 4),
+       (15, 8, 3),
+       (16, 8, 4),
+       (17, 9, 2),
+       (18, 9, 4),
+       (19, 10, 3),
+       (20, 10, 4),
+       (21, 11, 3),
+       (22, 11, 4),
+       (23, 12, 2),
+       (24, 12, 4),
+       (25, 13, 2),
+       (26, 13, 4),
+       (27, 14, 3),
+       (28, 14, 4),
+       (29, 15, 3),
+       (30, 15, 4),
+       (31, 16, 1),
+       (32, 16, 4),
+       (33, 17, 3),
+       (34, 17, 4),
+       (35, 18, 3),
+       (36, 18, 4),
+       (37, 19, 3),
+       (38, 19, 4),
+       (39, 20, 1),
+       (40, 20, 4),
+       (41, 21, 1),
+       (42, 21, 4);
 
--- ----
+INSERT INTO `user_info` (id, username, email, password)
+VALUES (1, '123', '1234567891@qq.com', '$2a$10$JOD0yzajuN.zC5a2mUaw6uq05DHOeDka9VFM4UWj4xodHAtlvE1O.');
 
-INSERT INTO `tag` (id, tag_name)
-VALUES (1, 'tag1');
-INSERT INTO `tag` (id, tag_name)
-VALUES (2, 'tag2');
-INSERT INTO `tag` (id, tag_name)
-VALUES (3, 'tag3');
+INSERT INTO `user_role` (id, role_code, role_name, is_del)
+VALUES (1, 'USER', 'USER', 0),
+       (2, 'ADMIN', 'ADMIN', 0),
+       (3, 'SUPER_ADMIN', 'SUPER_ADMIN', 0);
 
--- ----
-
-INSERT INTO `user_info` (id, username, password)
-VALUES (1, '123', '$2a$10$0Sav7AssgISibD2Kd3XTq.wfqMZ4aClgRcZOZqfaEuPn/.dLa4b4y');
-
--- ----
-
-INSERT INTO `user_role`
-VALUES (1, 'STUDENT', 'STUDENT', 0);
-
--- ----
-
-INSERT INTO `user_role_rel`
-VALUES (1, 1, 1, 0, DEFAULT);
+INSERT INTO `user_role_rel` (id, user_id, role_id, is_del)
+VALUES (1, 1, 1, 0),
+       (2, 1, 2, 0),
+       (3, 1, 3, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
